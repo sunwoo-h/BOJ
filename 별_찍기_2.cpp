75ee5f165896c3fa42265d795e735ec972a9a341
sunwoo-h <flexible8011@gmail.com>
2025-03-31 16:08:06 +0900

    별 찍기 2 : setw()함수 사용 추가

--- 별_찍기_2.cpp
+++ 별_찍기_2.cpp
@@ -1,3 +1,4 @@
+// 방법 1. 기본 for문 사용
 #include <iostream>
 using namespace std;
 
@@ -12,6 +13,25 @@ int main() {
       cout << "*";
     }
     cout << endl;
+  }
+
+  return 0;
+}
+
+
+// 방법 2. setw(n), string(n,c) 함수 사용
+// 방법 1보다 코드 간결
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= n; i++){
+    string s(i, '*');
+    cout << setw(n) << s << endl; // setw(n)은 출력 필드를 n칸으로 설정해서 오른쪽 정렬이 되게 해줌
   }
 
   return 0;
