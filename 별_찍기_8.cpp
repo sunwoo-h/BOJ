0bd6af9cd5ce8cebae257b4bd65ff7b7060b7c83
sunwoo-h <flexible8011@gmail.com>
2025-04-01 10:04:57 +0900

    별 찍기 8 : 코드 추가

--- 별_찍기_8.cpp
+++ 별_찍기_8.cpp
@@ -30,3 +30,32 @@ int main(){
   }
   return 0;
 }
+
+
+// 좋은 코드... 나누지 않고 한번에 생각한 거
+#include <iostream>
+#include <cmath>  // abs 함수 사용
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= 2 * N - 1; ++i) {
+        int stars = i <= N ? i : 2 * N - i;
+        int spaces = 2 * (N - stars);
+
+        // 왼쪽 별
+        for (int j = 0; j < stars; ++j) cout << '*';
+
+        // 공백
+        for (int j = 0; j < spaces; ++j) cout << ' ';
+
+        // 오른쪽 별
+        for (int j = 0; j < stars; ++j) cout << '*';
+
+        cout << '\n';
+    }
+
+    return 0;
+}
