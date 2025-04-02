1dcedcd209837e0348a547796cccb974172372f0
sunwoo-h <flexible8011@gmail.com>
2025-04-02 17:03:12 +0900

    8393

---
+++ 합.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+  int N;
+  int sum = 0;
+  cin >> N;
+  
+  for(int i = 1; i <= N; i++){
+    sum += i;
+  }
+
+  cout << sum;
+  return 0;
+}
