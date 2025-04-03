12225ecf3f18c71e8ca149c07ff9b097ded567ba
sunwoo-h <flexible8011@gmail.com>
2025-04-03 16:13:53 +0900

    10951

---
+++ A_B_4.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+
+  while (cin >> A >> B) {  // 입력이 잘 들어오면 true
+    // 이렇게 while문 조건부에 수식을 넣을 수도 있음!
+      cout << A + B << '\n';
+  }
+
+  return 0;
+}
