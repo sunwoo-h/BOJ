e015d64554ac4fe35dbe3fc82b99b52908821af6
sunwoo-h <flexible8011@gmail.com>
2025-04-05 12:22:54 +0900

    10991 주석 추가

--- 별_찍기_16.cpp
+++ 별_찍기_16.cpp
@@ -7,7 +7,7 @@ int main() {
   int space = N; // 이렇게 space 변수 따로 선언해서 써줘야 됨!! N 그대로 쓰면 오류
 
   for(int j = 1; j < N+1 ; j++){
-    cout << string(space-1, ' '); // 문자열 반복 sting 함수 적극 사용
+    cout << string(space-1, ' '); // 문자열 반복 sting 함수 적극 사용 // + 아님 걍 sting(N-i, ' ') 해도 되네.. -> 연산시 원래 있는 인수 적극 조합해보기
     int star = j;
     for(star ; star > 0 ; star--){
     cout << "* ";
@@ -16,3 +16,4 @@ int main() {
     space -= 1; // for 문 내에서 다른 인자의 증감 이용하여 반복 시키고 싶을 때 자주 사용 로직
   }
 }
+
