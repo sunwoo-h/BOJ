7325cfc49e7068e752e7840eab630784207dc53d
sunwoo-h <flexible8011@gmail.com>
2026-01-15 17:08:12 +0900

    2979

--- 1주차/트럭_주차.cpp
+++ 1주차/트럭_주차.cpp
@@ -44,6 +44,7 @@ int main() {
   for(int i = 0; i < 3; i++){
     cin >> bill[i];
   }
+  // 그냥 int A, B, C 선언해놓고, cin >> A >> B >> C 했어도 됨
   for(int i = 0; i < 6; i++){
     cin >> stamp[i];
   }
@@ -52,4 +53,27 @@ int main() {
   calculate();
 
   return 0;
+}
+
+
+
+// 깔끔 풀이(큰돌 답안)
+#include <bits/stdc++.h>
+using namespace std;   
+int A, B, C, a, b, cnt[104], ret;
+int main(){
+	cin >> A >> B >> C;  // 이렇게 입력받을 생각 하기
+	for(int i = 0; i < 3; i++){
+		cin >> a >> b; // 이렇게 입력받을 생각 하기2
+		for(int j = a; j < b; j++)cnt[j]++; 
+	}
+	for(int j = 1; j < 100; j++){
+		if(cnt[j]){ // if문 true 사고과정 익히기
+			if(cnt[j] == 1) ret += A;
+			else if(cnt[j] == 2)ret += B * 2;
+			else if(cnt[j] == 3)ret += C * 3;
+		}
+	}
+	cout << ret << "\n"; 
+    return 0;
 }
