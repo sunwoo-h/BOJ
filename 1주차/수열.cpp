3f2828f825f3330000cb9ea73b04646984601052
sunwoo-h <flexible8011@gmail.com>
2026-01-16 11:51:13 +0900

    2559

---
+++ 1주차/수열.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k, temp, ret = -10000000;
+int arr[1000004], psum[100001];
+
+
+int main (){
+
+  // 1.입력 받기s
+  cin >> n >> k;
+  
+
+  // 2. 구간합 psum 구하기
+  for(int i = 1; i <= n; i++){
+    cin >> temp;
+    psum[i] = psum[i-1] + temp;
+  }
+
+  // 3. psum 이용해서 결과값 저장
+  for(int i = k; i <= n; i++){
+    ret = max(ret, psum[i] - psum[i-k]); // 슬라이딩 윈도우
+  }
+
+  cout << ret << '\n';
+
+  return 0;
+}
