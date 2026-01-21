c71addeb66010f6ab04bad1078c6f507324a3be2
sunwoo-h <flexible8011@gmail.com>
2026-01-21 15:15:49 +0900

    DFS

---
+++ 개념/DFS.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int n = 6; // 정점이 5개라서 6으로 선언해줌
+vector<int> adj[n];
+int visitied[n];
+
+void DFS(int u){
+  visitied[u] = 1;
+  cout << u << '\n';
+  for(int v : adj[u]){ // u에 인접한 요소들 탐색
+    if(visitied[v] == 0){
+      DFS(v);
+    }
+  }
+  cout << u << "로부터 시작된 함수가 종료되었습니다.\n";
+  return;
+}
+
+int main() {
+  adj[1].push_back(2);
+  adj[1].push_back(3);
+  adj[2].push_back(4);
+  adj[4].push_back(2);
+  adj[2].push_back(5);
+  DFS(1);
+  return 0;
+}
