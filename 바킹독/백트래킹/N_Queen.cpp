fbe4f46f524d590338264d53b90519178cacbc0e
sunwoo-h <flexible8011@gmail.com>
2026-02-25 17:36:56 +0900

    9663

---
+++ 바킹독/백트래킹/N_Queen.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, cnt;
+bool colUsed[20]; // (열)
+bool diag1Used[40]; // row + col (대각선 /)
+bool diag2Used[40]; // row - col + (n-1) (대각선 \)
+
+void go(int row){ // 행을 인자로 받아서 반복
+  if(row == n){
+    cnt++;
+    return;
+  }
+
+  for(int col = 0; col < n; col++){ // 열마다 반복
+    if(colUsed[col]) continue;
+    if(diag1Used[row + col]) continue;
+    if(diag2Used[row - col + (n - 1)]) continue;
+
+    // 퀸 배치
+    colUsed[col] = true;
+    diag1Used[row + col] = true;
+    diag2Used[row - col + (n - 1)] = true;
+
+    go(row + 1);
+
+    // 원복
+    colUsed[col] = false;
+    diag1Used[row + col] = false;
+    diag2Used[row - col + (n - 1)] = false;
+  }
+}
+
+int main(){
+  cin >> n;
+  go(0);
+  cout << cnt;
+  return 0;
+}
