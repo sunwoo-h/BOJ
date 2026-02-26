836d43cb7bdcf507aed1622ba6f936d3359c0fdd
sunwoo-h <flexible8011@gmail.com>
2026-02-26 18:21:26 +0900

    15657

---
+++ 바킹독/백트래킹/N과_M_8.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+int a[10], ret[10];
+
+void go(int depth, int start){
+  if(depth == m){
+    for(int i = 0; i < m; i++){
+      cout << ret[i] << ' '; 
+    }
+    cout << '\n';
+    return;
+  }
+
+  for(int i = start; i < n; i++){
+    ret[depth] = a[i]; // 결과 저장
+    go(depth+1,i); // 비내림차순이라 i로 넘김
+  }
+}
+
+int main(){
+  cin >> n >> m;
+
+  for(int i = 0; i < n; i++){
+    cin >> a[i];
+  }
+
+  sort(a,a+n); // 오름차순 정렬
+  
+  go(0,0);
+
+
+  return 0;
+}
